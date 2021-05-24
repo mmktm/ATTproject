4514c152c11d4aeb44171c1e333ecf03dfa7a6bf
mmktm <kettamas.8065@gmail.com>
2021-05-24 16:25:53 +0700

    login regit admin

---
+++ loginadmin.php
@@ -0,0 +1,35 @@
+<?php 
+header("content-type:text/javascript;charset=utf-8"); //ภาษาไทย
+header('content-type:application/json'); //ใช้ข้อมูลแบบ json
+include 'connect.php'; //เชื่อมต่อDATABASE cloud
+
+    $Email = $_POST['Email'];
+    $Passwordd = $_POST['Passwordd'];
+
+    $userdata = array();
+    
+
+    $sql_checkloginad = " SELECT * FROM `admin` 
+                        WHERE Email = '$Email' AND Passwordd = '$Passwordd'" ;
+        
+        $result_checkloginad = $link->query($sql_checkloginad);
+        $userdata = $result_checkloginad->fetch_assoc();
+
+            if($result_checkloginad->num_rows == 1 ){
+
+                echo json_encode("Login Success");
+                // echo json_encode($userdata,JSON_NUMERIC_CHECK);
+                // while ($row_checklogin = $result_checklogin->fetch_assoc()){
+                //     $output_checklogin[] = $row_checklogin ;
+                //     $j_checklogin = json_encode($output_checklogin);
+                // }
+                // echo "$j_checklogin\n" ;
+
+            }else{
+                echo json_encode("error");
+                // echo "error ".mysqli_error($link)."\n" ;
+            }
+
+mysqli_close($link);
+
+?>
---
+++ regisadmin.php
@@ -0,0 +1,67 @@
+<?php 
+header("content-type:text/javascript;charset=utf-8"); //ภาษาไทย
+header('content-type:application/json'); //ใช้ข้อมูลแบบ json
+date_default_timezone_set('Asia/Bangkok');//timezone
+include 'connect.php'; //เชื่อมต่อDATABASE cloud
+
+
+    $Username = $_POST['Username'];
+    $Passwordd = $_POST['Passwordd'];
+    $Email = $_POST['Email'];
+    $Date_User = date("Y-m-d") ;
+    $Time_User = date("H:i:s") ;
+
+        // Variable to check
+        // $Email = "john.doe@example.com";
+
+        // Remove all illegal characters from email
+        $Email = filter_var($Email, FILTER_SANITIZE_EMAIL);
+
+        // Validate e-mail
+        if (!filter_var($Email, FILTER_VALIDATE_EMAIL) === false) {
+            // echo("$Email is a valid email address\n");
+                
+            $sql_checkregisad = " SELECT * FROM `admin` 
+                                WHERE Username = '$Username' OR Email = '$Email'" ;
+
+            $result_checkregisad = $link->query($sql_checkregisad);
+
+            if($result_checkregisad->num_rows == 1 ){
+                // echo json_encode("error : username or email already exists"); 
+                echo json_encode("error"); 
+
+            }else{
+                
+                $sql_insertregisad = "INSERT INTO `admin` (Username, Passwordd , Email,Date_Admin,Time_Admin)
+                                    VALUES ('$Username','$Passwordd','$Email','$Date_Admin','$Time_Admin')" ; //.md5 เข้ารหัส
+
+                    $result_insertregisad = $link->query($sql_insertregisad);
+                    if($result_insertregisad){
+                        echo json_encode("success");
+                    }
+            }
+        
+        } else {
+            echo("$Email is not a valid email address\n");
+        }
+    // $sql_checkregis = " SELECT * FROM `user` 
+    //                     WHERE Username = '$Username' OR Email = '$Email'" ;
+        
+    //     $result_checkregis = $link->query($sql_checkregis);
+
+    //         if($result_checkregis->num_rows == 1 ){
+    //             echo json_encode("error"); 
+    //         }else{
+                
+    //             $sql_insertregis = "INSERT INTO user (Username, Passwordd , Email, Status_User,Date_User,Time_User)
+	// 			                    VALUES ('$Username','$Passwordd','$Email','$Status_User','$Date_User','$Time_User')" ; //.md5 เข้ารหัส
+
+    //                 $result_insertregis = $link->query($sql_insertregis);
+    //                 if($result_insertregis){
+    //                     echo json_encode("success");
+    //                 }
+    // }
+
+mysqli_close($link);
+
+?>
