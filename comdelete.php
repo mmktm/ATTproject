8a32f34921eac0e90061bcae96eb2bda3e6c73bf
mmktm <kettamas.8065@gmail.com>
2021-07-17 22:42:30 +0700

    profileedit

--- comdelete.php
+++ comdelete.php
@@ -5,22 +5,60 @@ date_default_timezone_set('Asia/Bangkok');//timezone
 include 'connect.php'; //เชื่อมต่อDATABASE cloud
 
             //comdelete
-            //รับค่า ID_Comment จากตัวแปร $_POST['comdelete'] อาจจะเป็นรูปถังขยะ
-            if(isset($_POST['comdelete']) && $_POST['comdelete'] != '' ){ //รับค่าตัวแปร comdelete และ comdelete ต้องไม่ใช่ค่าว่าง
+            //รับค่า ID_Comment จากตัวแปร $_POST['idcomdel'] อาจจะเป็นรูปถังขยะ
+            //รับ 2 ค่า idcom and iduser
+            if(isset($_POST['idcomdel']) && $_POST['idcomdel'] != '' ){ //idcomment ที่จะ delete
 
-                $idcomment = $_POST['comdelete'] ;
-                $statuscomdelete = '0';
+                $idusercomdel = $_POST['idusercomdel'];
+                $idcomment = $_POST['idcomdel'] ;
+                $statuscomdel = '0';
             
-                $sql_comdelete = " UPDATE `comment` 
-                                    SET Status_Comment = '0' 
-                                    WHERE ID_Comment = '$idcomment'" ;
-                    
-                    $result_comdelete = $link->query($sql_comdelete);
-                    if($result_comdelete){
-                        echo "result_comdelete is true \n"; }
-                    else{
-                        echo "result_comdelete is false ".mysqli_error($link)."\n" ;
-                        }
+                $sql_comdel = " UPDATE `comment` 
+                                SET Status_Comment = '$statuscomdel'
+                                WHERE ID_Comment = '$idcomment' AND Status_Comment = '1' " ;
+                                
+                    $result_comdel = $link->query($sql_comdel);
+                    // echo $result_comdel."\n";
+                    echo $link->query($sql_comdel);
+
+                    // if($result_comdel == '1' ){
+                        
+                    //     //ค่าidcontent
+                    //     $sql_idcontent = " SELECT ID_Content
+                    //                         FROM `comment` 
+                    //                         WHERE ID_Comment = '$idcomment' && ID_User = '$idusercomdel' ";
+                    //         $result_idcontent = $link->query($sql_idcontent);
+                    //         $row_idcontent = $result_idcontent->fetch_assoc();
+                    //         $idcontent = $row_idcontent['ID_Content'];
+
+                    //     //ดึงค่าtotalcom
+                    //     $sql_totalcom = " SELECT
+                    //                             ID_Content,
+                    //                             Total_Com 
+                    //                         FROM
+                    //                             content 
+                    //                         WHERE
+                    //                             ID_Content = '$idcontent' ";
+
+                    //         $result_totalcom = $link->query($sql_totalcom);
+                    //         $row_totalcom = $result_totalcom->fetch_assoc();
+                    //         $totalcom = $row_totalcom['Total_Com'];
+
+                    //     //ลบจำนวน totalcom in table content
+                    //     $sql_deltotalcom = " UPDATE content
+                    //                          SET Total_Com = '$totalcom'-1
+                    //                          WHERE ID_Content = '$idcontent' ";
+                    //         $result_deltotalcom = $link->query($sql_deltotalcom);
+
+                    //         if($result_deltotalcom == '1'){
+                    //             echo "result_comdelete is true \n"; }
+                    //         else{
+                    //             echo "result_comdelete is false ".mysqli_error($link)."\n" ;
+                    //             }
+           
+                    // }else{
+                    //     echo "error result_commdel ".mysqli_error($link)."\n" ;
+                    //     }
             }
 
 mysqli_close($link);
---
+++ profileedit.php
@@ -0,0 +1,35 @@
+<?php 
+header("content-type:text/javascript;charset=utf-8"); //ภาษาไทย
+header('content-type: application/json'); //ใช้ข้อมูลแบบ json
+include 'connect.php'; //เชื่อมต่อDATABASE cloud
+
+    //input id-user
+    if(isset($_POST['ID_User']) && $_POST['ID_User'] != '') {
+        
+        $ID_User = $_POST['ID_User']; //iduser
+        $Username = $_POST['Username'];
+        $Image = $_FILES['Image']['name'];     
+        $tmp_name = $_FILES['Image']['tmp_name'];
+        $ImagePath = 'uploadimages/'.$Image;
+        move_uploaded_file($tmp_name,$ImagePath);
+        
+                    //Profile edit (update)
+                    $sql_pfedit = " UPDATE user
+                                        SET 
+                                            Username = '$Username',
+                                            Image = '$Image'
+                                        WHERE
+                                            ID_User = '$ID_User' && Status_User = 'Active' " ;
+                
+                        $result_pfedit = $link->query($sql_pfedit);
+
+                            if($result_pfedit){
+                                echo "result_pfedit is true \n"; }
+                            else{
+                                echo "result_pfedit is false (Status-user) ".mysqli_error($link)."\n" ;
+                            }
+            }else{echo "error if" ; }
+    
+    mysqli_close($link);
+
+    ?>
