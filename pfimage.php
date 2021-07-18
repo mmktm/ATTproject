cf4ef7ab8e0c32c48c90f91377ae470901cf2021
mmktm <kettamas.8065@gmail.com>
2021-07-18 18:28:11 +0700

    pfimage

--- pfimage.php
+++ pfimage.php
@@ -8,24 +8,40 @@ $response = new stdClass;
 $response->status = null ;
 $response->message = null ;
 
-$destination_dir = "avatar/";
-$base_filename = basename($_FILES["file"]["name"]);
-$target_file = $destination_dir.$base_filename;
+if(isset($_POST['ID_User']) && $_POST['ID_User'] != '') {
+        
+    $ID_User = $_POST['ID_User']; //iduser
 
-if(!$_FILES["file"]["error"]){
-    
-    if(move_uploaded_file($_FILES["file"]["tmp_name"],$target_file)){
-        $response->status = true;
-        $response->message = "File uploaded succcessfully";
+    $destination_dir = "avatar/";
+    $base_filename = basename($_FILES["file"]["name"]);
+    $target_file = $destination_dir.$base_filename;
 
+    if(!$_FILES["file"]["error"]){
+        
+        if(move_uploaded_file($_FILES["file"]["tmp_name"],$target_file)){
+            $response->status = true;
+            $response->message = "File uploaded succcessfully";
+
+                //Profile edit (update)
+                $sql_pfedit = " UPDATE user SET Image = '$base_filename' 
+                                WHERE ID_User = '$ID_User' && Status_User = 'Active' " ;
+
+                    $result_pfedit = $link->query($sql_pfedit);
+
+                    if($result_pfedit){
+                        echo "result_pfedit is true \n"; }
+                    else{
+                        echo "result_pfedit is false (Status-user) ".mysqli_error($link)."\n" ;
+                    }
+        }else{
+            $response->status = false;
+            $response->message = "File uploaded failed";
+            
+        }
     }else{
         $response->status = false;
         $response->message = "File uploaded failed";
-        
     }
-}else{
-    $response->status = false;
-    $response->message = "File uploaded failed";
 }
 
 echo json_encode($response);
